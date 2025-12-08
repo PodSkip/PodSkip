ce50da644e485f381af0eb36d4760b3694069161
smp46 <me@smp46.me>
2025-12-08 15:54:25 +1000

    chore(setup): init app w/expo nativewind gluestack

---
+++ podskip-app/app/(tabs)/index.tsx
@@ -0,0 +1,9 @@
+import { Button, ButtonText } from '@/components/ui/button';
+
+export default function HomeScreen() {
+  return (
+    <Button className="justify-center h-12 w-32 items-center bg-blue-500 rounded-md">
+      <ButtonText>hello</ButtonText>
+    </Button>
+  );
+}
